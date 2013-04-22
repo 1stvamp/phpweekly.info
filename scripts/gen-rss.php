f4fab2609650393354b7c1a1e821a7a738f53429
Wes Mason <wes@1stvamp.org>
2013-04-22 01:16:24 +0100

    Add script to generate RSS feed and latest RSS feed index.xml

---
+++ scripts/gen-rss.php
@@ -0,0 +1,35 @@
+<?php
+$base_uri = 'http://phpweekly.info/archive/';
+$base_dir = dirname(__FILE__) . '/../';
+
+require $base_dir . 'vendor/autoload.php';
+
+use \Suin\RSSWriter as Rss;
+
+$dom = simplexml_import_dom(HTML5_Parser::parse(file_get_contents($base_dir . 'archive/index.html')));
+
+$feed = new Rss\Feed;
+$channel = new Rss\Channel;
+$channel
+	->title('PHP Weekly')
+	->description('Archived PHP Weekly issues')
+	->url($base_uri)
+	->appendTo($feed);
+
+
+foreach($dom->body->div[0]->div[0]->section[0]->ul[0]->li as $li) {
+	$title = (string)$li->a;
+	$link = $base_uri . $li->a['href'];
+	$parts = explode(' for ', $title);
+	$parts = explode(' the ', $parts[1]);
+	$date = strtotime(str_replace(' of', '', $parts[1]));
+	$item = new Rss\Item;
+	$item
+		->title($title)
+		->description($title)
+		->url($link)
+		->pubDate($date)
+		->appendTo($channel);
+}
+echo 'Updating ' . $base_dir . 'archive/index.xml' . PHP_EOL;
+file_put_contents($base_dir . 'archive/index.xml', (string)$feed);
