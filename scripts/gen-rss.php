d41308f224bb46b6e1551ebbd7178bb93b08b9e4
Wes Mason <wes@1stvamp.org>
2013-05-05 22:48:01 +0100

    Add full articles to RSS output

--- scripts/gen-rss.php
+++ scripts/gen-rss.php
@@ -1,3 +1,4 @@
+#!/usr/bin/env php
 <?php
 $base_uri = 'http://phpweekly.info/archive/';
 $base_dir = dirname(__FILE__) . '/../';
@@ -23,9 +24,10 @@ foreach($dom->body->div[0]->div[0]->section[0]->ul[0]->li as $li) {
 	$parts = explode(' the ', $parts[1]);
 	$date = strtotime(str_replace(' of', '', $parts[1]));
 	$item = new Rss\Item;
+	$subDoc = simplexml_import_dom(HTML5_Parser::parse(file_get_contents($base_dir . 'archive/' . $li->a['href'])));
 	$item
 		->title($title)
-		->description($title)
+		->description((string)$subDoc->body->asXML())
 		->url($link)
 		->pubDate($date)
 		->appendTo($channel);
